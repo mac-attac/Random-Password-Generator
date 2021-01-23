55e239ea726c22cbafbb2e715ac1d63432064bf0
Steve McNamara <stephenmcnamara36@gmail.com>
2021-01-23 10:21:52 -0700

    fixed userInput() and added getRandom() and generatePassword()

--- script.js
+++ script.js
@@ -9,60 +9,81 @@ function writePassword() {
   passwordText.value = password;
 }
 
-function correctLength() {
+function userInput() {
   var lengthPassword = prompt("How many characters between 8 and 128 would you like?")
 
   if (lengthPassword >= 8 && lengthPassword <=128) {
-    return true;
+    true;
   } else {
     alert("You do not have the required number of characters")
-    correctLength()
+    userInput()
   }
+  var lower = confirm("Would you like lowercase letters included?")
+  var upper = confirm("Would you like uppercase letters included?")
+  var numeric= confirm("Would you like numbers included?")
+  var special = confirm("Would you like special characters included?")
+  
+  //if no characters were specified, run it again
+  if (!lower && !upper && !numeric && ! special) {
+    alert("You did not choose any characters! Feel free to start over!")
+    userInput()
+  }
+  var userAnswers = {
+    length : lengthPassword,
+    wantsUpper : upper,
+    wantsLower : lower,
+    wantsNumeric : numeric,
+    wantsSpecial : special,
+  }
+  return userAnswers
 }
-
-//created function generate password
+  //add prompts for password criteria
+  
+function getRandom (arr) {
+    var randomIndex= Math.floor(Math.random() * arr.length);
+    return arr[randomIndex]
+}
+  
+  
+  //created function generate password
 function generatePassword() {
-//add variables
-var lowercase = "abcdefghijklmnopqrstuvwxyz"
-var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var numbers = "0123456789"
-var special = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
-//add prompts for password criteria
+    //add variables
+  var options = userInput()
+  var chars = {
+  lower : "abcdefghijklmnopqrstuvwxyz".split(""),
+  upper : "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split(""),
+  num : "0123456789".split(""),
+  spec : "!#$%&'()*+,-./:;<=>?@[^_`{|}~".split(""),
+  }
 
-//length of password
-correctLength()
+  //empty arrays for adding desired characters and then final result
+  var result = []
+  var possibleChars = []
 
-// //lowercase prompt
-// var lowercase = confirm("Would you like lowercase letters included?")
-
-// if (true) {
-//   //include lowercase letters
-//   Math.random() * lowercase.length
-// } else //do not include lowercase letters
-
-// //uppercase prompt
-// var uppercase = confirm("Would you like uppercase letters included?")
-
-// if (true) {
-//   //include uppercase letters
-// } else //do not include lowercase letters
-
-// //numeric prompt
-// var numeric = confirm("Would you like numbers included?")
-
-// if (true) {
-//   //include numbers letters
-// } else //do not include lowercase letters
-
-// //special characters prompt
-// confirm("Would you like special characters included?")
-
-// if (true) {
-//   //include lowercase letters
-// } else //do not include lowercase letters
+  //create if statements to append desired characters onto result
+  if (options.wantsLower) {
+    possibleChars = possibleChars.concat(chars.lower);
+  }
+  
+  if (options.wantsUpper) {
+    possibleChars = possibleChars.concat(chars.upper);
+  }
+  if (options.wantsNumeric) {
+    possibleChars = possibleChars.concat(chars.num);
+  }
+    if (options.wantsSpecial) {
+    possibleChars = possibleChars.concat(chars.spec);
+  }
+  
+  while (result.length < options.length) {
+    result.push(getRandom(possibleChars))
+  }
 
 
+  return result.join("")
 }
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
